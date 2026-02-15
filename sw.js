57834b98e81659a469d99247894c979c04aec129
blaybelmahmud <blaybelmahmud@gmail.com>
2026-02-15 19:31:29 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'mahmud-v1';
+const assets = [
+'./',
+'./index.html',
+'./manifest.json'
+];
+
+self.addEventListener('install', event => {
+event.waitUntil(
+caches.open(cacheName).then(cache => {
+return cache.addAll(assets);
+})
+);
+});
+
+self.addEventListener('fetch', event => {
+event.respondWith(
+caches.match(event.request).then(response => {
+return response || fetch(event.request);
+})
+);
+});
